0346fb9eade95da4bd87ee0c80cef9a58502ef61
Yelizaveta <yelisavetatpu@gmail.com>
2016-03-16 20:30:39 +0000

    basic application skeleton created

---
+++ js/controllers/ListCtrl.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function (angular) {
+  angular
+    .module("ProgrammesListApp")
+    .controller('ListCtrl', Controller)
+  ;
+
+  Controller.$inject = [];
+
+  function Controller() {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+
+    }
+  }
+
+}(angular));
