2f07c98a6eac40abd85c5664b14438dd86878fc0
Yelizaveta <yelisavetatpu@gmail.com>
2016-03-17 06:24:21 +0000

    methods for selecting tabs in menu added to controller

--- js/controllers/ListCtrl.js
+++ js/controllers/ListCtrl.js
@@ -11,11 +11,17 @@
   function Controller() {
     var vm = this;
     vm.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    vm.selectedTab = vm.alphabet[0];
+    vm.selectTab = selectTab;
 
     activate();
 
     function activate() {
 
+    }
+
+    function selectTab(tab) {
+      vm.selectedTab = tab;
     }
   }
 
--- tests/controllers/ListCtrl.spec.js
+++ tests/controllers/ListCtrl.spec.js
@@ -20,6 +20,21 @@
       expect(ctrl.alphabet).toEqual(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']);
     });
 
+    it("should initially set selected tab to 'a'", function() {
+      expect(ctrl.selectedTab).toBeDefined();
+      expect(ctrl.selectedTab).toEqual("a");
+    });
 
+    it("should have method to change selected tab", function() {
+      expect(ctrl.selectTab).toBeDefined();
+      expect(typeof ctrl.selectTab).toBe("function");
+    });
+
+    it("should be able to change selected tab", function() {
+      var tab = "b";
+      ctrl.selectTab(tab);
+
+      expect(ctrl.selectedTab).toEqual(tab);
+    });
   });
 }());
